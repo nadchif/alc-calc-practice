f08350818c68867b8cfa941eb7231f95a24e03c1
unknown <nadchif>
2019-08-13 02:16:31 +0200

    Performace tweaks

--- assets/app.js
+++ assets/app.js
@@ -102,7 +102,7 @@ const doMathOp = (event, entry) => {
 
 const bootstrap = () => {
     document.querySelectorAll('.btn').forEach((button) => {
-        button.addEventListener('mousedown', (event) => doMathOp(event, button.textContent))
+        button.addEventListener('mouseup', (event) => doMathOp(event, button.textContent))
     });
     document.querySelector('body').addEventListener("keydown", () => {
         doMathOp(event, event.key);
